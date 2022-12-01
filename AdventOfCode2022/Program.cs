3da8cae1004fc9bfbcbe4632176e3244633402b4
Fudgey <fudgebucket27@gmail.com>
2022-12-01 15:17:41 +1000

    part one solved

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day1;
+
+List<Elf> elves = new List<Elf>();
+Elf elf = new Elf();
+foreach (string line in System.IO.File.ReadLines("input.txt"))
+{ 
+    if(string.IsNullOrEmpty(line))
+    {
+        elf.TotalCalories = elf.Calories!.Sum();
+        elves.Add(elf);
+        elf = new Elf();
+    }
+    else
+    {
+     
+        elf.Calories!.Add(Int32.Parse(line));
+    }
+}
+
+var highestCalories = elves.Max(t=> t.TotalCalories);
+Console.WriteLine($"Elf with highest calories was {highestCalories}");
