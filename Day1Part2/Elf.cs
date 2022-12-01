44c82cf44b07faaab814b09bf251adc89f1770e7
Fudgey <fudgebucket27@gmail.com>
2022-12-01 15:26:25 +1000

    day 1 part 1 done

---
+++ Day1Part2/Elf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Elf
+    {
+        public List<int>? Calories { get; set; } = new List<int>();
+        public int TotalCalories { get; set; }
+    }
+}
---
+++ Day1Part2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Day1;
+
+List<Elf> elves = new List<Elf>();
+Elf elf = new Elf();
+foreach (string line in System.IO.File.ReadLines("input.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elf.TotalCalories = elf.Calories!.Sum();
+        elves.Add(elf);
+        elf = new Elf();
+    }
+    else
+    {
+
+        elf.Calories!.Add(Int32.Parse(line));
+    }
+}
+
+var elvesSorted = elves.OrderByDescending(x=>x.TotalCalories).ToList();
+int highestCaloriesTop3 = elvesSorted[0].TotalCalories + elvesSorted[1].TotalCalories + elvesSorted[2].TotalCalories;
+Console.WriteLine($"Top 3 elves hold {highestCaloriesTop3}");
