8fbe4c31768cbbbf6ad93108682b36301b8b61c0
Fudgey <fudgebucket27@gmail.com>
2022-12-02 16:30:21 +1000

    boiler plate

---
+++ Day2Part1/Program.cs
@@ -0,0 +1,9 @@
+﻿List<string> moves = new List<string>();
+using (StreamReader streamReader = new StreamReader("input.txt"))
+{
+    string move;
+    while ((move = streamReader.ReadLine()!) != null)
+    {
+        moves.Add(move); // Add to list.     
+    }
+}
