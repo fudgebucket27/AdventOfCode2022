6a3b2d14dd04c1b087ffdd55bce8a47b4f1115ea
Fudgey <fudgebucket27@gmail.com>
2022-12-02 19:02:07 +1000

    day 2 part 1 done

--- Day2Part1/Program.cs
+++ Day2Part1/Program.cs
@@ -7,3 +7,49 @@ using (StreamReader streamReader = new StreamReader("input.txt"))
         moves.Add(move); // Add to list.     
     }
 }
+
+int score = 0;
+
+foreach(var move in moves)
+{
+    string opponentMove = move.Split(' ')[0];
+    string myMove = move.Split(' ')[1];
+    if(opponentMove == "A" && myMove == "X") //rock and rock. draw
+    {
+        score += 1 + 3;
+    }
+    else if (opponentMove == "A" && myMove == "Y") //rock and paper. win
+    {
+        score += 2 + 6;
+    }
+    else if (opponentMove == "A" && myMove == "Z") //rock and scissors. lose
+    {
+        score += 3 + 0;
+    }
+    else if (opponentMove == "B" && myMove == "X") //paper and rock. lose
+    {
+        score += 1 + 0;
+    }
+    else if (opponentMove == "B" && myMove == "Y") //paper and paper. draw
+    {
+        score += 2 + 3;
+    }
+    else if (opponentMove == "B" && myMove == "Z") //paper and scissors. win
+    {
+        score += 3 + 6;
+    }
+    else if (opponentMove == "C" && myMove == "X") //scissors and rock. win
+    {
+        score += 1 + 6;
+    }
+    else if (opponentMove == "C" && myMove == "Y") //scissors and paper. lose
+    {
+        score += 2 + 0;
+    }
+    else if (opponentMove == "C" && myMove == "Z") //scissors and scissors. draw
+    {
+        score += 3 + 3;
+    }
+}
+
+Console.WriteLine($"Total score {score}");
