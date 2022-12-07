125870ba43dd92b803f4061ff8da72fb87689b75
Fudgey <fudgebucket27@gmail.com>
2022-12-08 08:53:03 +1000

    kind of working

---
+++ Day7Part1/Folder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Part1
+{
+    public class Folder
+    {
+       public string Path { get; set; }
+       public List<File> Files { get; set; } = new List<File>();
+    }
+
+    public class File
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
---
+++ Day7Part1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Day7Part1;
+
+string[] input = System.IO.File.ReadAllLines("input.txt");
+
+List<Folder> directory = new List<Folder>();
+string currentDirectory = "";
+var currentCommand = "";
+foreach(string line in input)
+{
+    var command = line.Split(' ');
+    if (line[0] == '$')
+    {
+        if(command[1] == "cd" && command[2] != "..")
+        {
+            currentDirectory += "/" + command[2] ;
+            if(!directory.Where(x=> x.Path == currentDirectory).Any())
+            {
+                Folder folder = new Folder();
+                folder.Path = currentDirectory;
+                directory.Add(folder);
+            }
+            currentCommand = "cd";
+        }
+        else if (command[1] == "cd" && command[2] == "..")
+        {
+            currentDirectory = String.Concat(currentDirectory.Split('/').Take(currentDirectory.Split('/').Length - 1).ToArray());
+            currentCommand = "cd";
+        }
+        else if(command[1] == "ls")
+        {
+            currentCommand = "ls";
+        }
+    }
+    if (currentCommand == "ls" && command[0] != "$")
+    {
+       
+        if(command[0] != "dir") // dont care about dir unless we navigate inside
+        {
+            var fileSize = long.Parse(command[0]);
+            var fileName = command[1];
+            Day7Part1.File file = new Day7Part1.File();
+            file.Name = fileName;
+            file.Size= fileSize;
+            if(directory.Where(x=> x.Path == currentDirectory && x.Files.Where(x=>x.Name==fileName).Any()).Any() == false)
+            {
+                directory.First(x => x.Path == currentDirectory).Files.Add(file);
+            }
+        }
+    }
+}
+var totalSize = directory.Sum(x=> x.Files.Sum(x=> x.Size));
+Console.WriteLine(totalSize);
